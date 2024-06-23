fd156992a679841f02cd9747077e3d2219f0d2ec
Surajbhan <suraj.civil@gmail.com>
2024-06-23 20:38:10 +0200

    quiz app basic layout done

--- 02-quiz_app/script/02-quiz_app.js
+++ 02-quiz_app/script/02-quiz_app.js
@@ -1,3 +1,44 @@
 
 
-console.log('Hi From Default');
+console.log('Hi From Quiz');
+
+const quizData = [
+  {
+    qtn: 'Roughly when did internet come into existence ?',
+    a:'1990',
+    b:'1983',
+    c:'1972',
+    d:'1984',
+    correct: 'b'
+  }, {
+    qtn: 'Which is the most used programming language in 2024 ?',
+    a:'JavaScript',
+    b:'C++',
+    c:'Python',
+    d:'MATLAB',
+    correct: 'c'
+  }, {
+    qtn: 'No. 1 super computer in 2024 ?',
+    a:'FRONTIER',
+    b:'AURORA',
+    c:'EAGLE',
+    d:'Non of the above',
+    correct: 'a'
+  }, {
+    qtn: 'When was Javascript invented ?',
+    a:'1983',
+    b:'1890',
+    c:'2000',
+    d:'1995',
+    correct: 'd'
+  }, {
+    qtn: ' What is HTML full form ?',
+    a:'High Markup Language',
+    b:'Hyper text Market Land',
+    c:'Hypertext Markup Language',
+    d:'Non of the above',
+    correct: 'c'
+  }
+];
+
+console.log(quizData);

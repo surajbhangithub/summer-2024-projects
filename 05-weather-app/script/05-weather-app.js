ddb12ed65206bd68f6f7883f3d3032e7bd6884b6
Surajbhan <suraj.civil@gmail.com>
2024-08-11 20:31:11 +0200

    Weather app with own CSS try

--- 05-weather-app/script/05-weather-app.js
+++ 05-weather-app/script/05-weather-app.js
@@ -1,3 +1,31 @@
 
 
-console.log('Hi From Default');
+// console.log('Weather app');
+
+
+const APIKEY = '6eada7deead428c02f8ff63626d07739';
+
+const url = (cityLocation) => 
+  `https://api.openweathermap.org/data/2.5/weather?q=${cityLocation}&appid=${APIKEY}`;
+
+
+async function getWeatherByCityLocation(cityLocation) {
+
+  const resp = await fetch(url(cityLocation),{origin:'cors'});
+
+  const respData = await resp.json();
+
+  console.log(respData);
+
+  console.log(ktoC(respData.main.temp));
+  
+}
+
+getWeatherByCityLocation('Halmstad');
+
+function ktoC (K) {
+
+  return (K-273.15).toFixed(1);
+
+}
+
